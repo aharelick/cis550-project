ca10e17f48f5e5485c07c95d9acab87c10046fa3
Alex Harelick <alexharelick@gmail.com>
2016-05-02 18:25:53 -0400

    added a ton of requirements, login page errors working

--- nessie/app.js
+++ nessie/app.js
@@ -1,24 +1,76 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var session = require('express-session');
+var mongoose = require('mongoose');
+var MongoStore = require('connect-mongo')(session);
+var flash = require('connect-flash');
+var expressValidator = require('express-validator');
+require('./config/pass')(passport);
 
 var routes = require('./routes/index');
 
 var app = express();
 
+/**
+ * Load environment variables.
+ */
+
+var config;
+if (app.get('env') === 'development') {
+  config = require('./config/config');
+} else {
+  config = {
+    MONGODB_URI: process.env.MONGODB_URI,
+    SESSION_SECRET: process.env.SESSION_SECRET
+  }
+}
+
+/**
+ * Connect to MongoDB.
+ */
+
+mongoose.connect(config.MONGODB_URI);
+mongoose.connection.on('error', function() {
+  console.error('MongoDB Connection Error. Please make sure that MongoDB is running.');
+});
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
 app.use(cookieParser());
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  store: new MongoStore({
+    url: config.MONGODB_URI,
+    autoReconnect: true
+  }),
+  secret: config.SESSION_SECRET,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+app.use(function(req, res, next) {
+  res.locals.messages = {
+    success: req.flash('success'),
+    errors: req.flash('errors'),
+    form_errors: req.flash('form-errors'),
+    form_errors_bool: req.flash('form-errors').length !== 0
+  };
+  next();
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
--- nessie/routes/index.js
+++ nessie/routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
+var passport = require('passport');
 var router = express.Router();
+var User = require('../models/User');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  if (req.isAuthenticated()) {
+    return res.redirect('/user/dashboard');
+  } else {
+    return res.redirect('/login');
+  }
+});
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  return res.render('login', { title: 'Login' });
+});
+
+/* POST login. */
+router.post('/login', function(req, res, next) {
+  passport.authenticate('local', function(err, user, info) {
+    if (err) { return next(err); }
+    if (!user) {
+      req.flash('errors', 'Incorrect email or password.');
+      return res.redirect('/login');
+    }
+    req.login(user, function(err) {
+      if (err) { return next(err); }
+      return res.redirect('/user/dashboard');
+    });
+  })(req, res, next);
 });
 
 module.exports = router;
