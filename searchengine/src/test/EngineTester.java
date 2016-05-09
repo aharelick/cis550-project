2b0ea27da540496801cb8301a047bafa154c63f1
Max Tromanhauser <mtromanhauser@gmail.com>
2016-05-09 14:35:45 -0400

    Java search stuff

--- searchengine/src/test/EngineTester.java
+++ searchengine/src/test/EngineTester.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import java.util.Set;
 
 import engine.Query;
+import engine.Query.Tuple;
 import search.Graphable;
 import test.GraphTester.TestNode;
 
@@ -20,41 +21,50 @@ public class EngineTester {
 		TestNode c = new TestNode("c");
 		TestNode d = new TestNode("d");
 		TestNode e = new TestNode("e");
+		TestNode f = new TestNode("f");
 		
 		a.addAdj(b);
-		a.addAdj(c);
-		a.addAdj(d);
 		
 		b.addAdj(a);
 		b.addAdj(c);
-		b.addAdj(e);
+		b.addAdj(d);
 		
-		c.addAdj(a);
 		c.addAdj(b);
-		c.addAdj(e);
+		c.addAdj(d);
 		
-		d.addAdj(a);
+		d.addAdj(b);
+		d.addAdj(c);
 		d.addAdj(e);
 		
-		e.addAdj(b);
-		e.addAdj(c);
 		e.addAdj(d);
+		e.addAdj(f);
+		
+		f.addAdj(e);
 		
 		Map<String, List<Graphable>> words = new HashMap<String, List<Graphable>>();
 		List<Graphable> list = new ArrayList<Graphable>();
 		list.add(a);
 		list.add(b);
-		list.add(c);
 		words.put("A", list);
 		list = new ArrayList<Graphable>();
-		list.add(d);
-		list.add(e);
+		list.add(f);
+		list.add(c);
 		words.put("B", list);
+		list = new ArrayList<Graphable>();
+		list.add(d);
+		words.put("C", list);	
+		
+		System.out.println("Cross Product:");
+		for (Tuple<Graphable, Graphable> pair : Query.getCartisian(words)) {
+			System.out.println(pair);
+		}
+		
 		
 		System.out.println("Paths:");
 		for (List<Graphable> path : Query.kShortestPaths(Query.getCartisian(words), 1000)) {
 			System.out.println(path);
 		}
+		
 		
 	}
 
